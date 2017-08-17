1f7f2fd22ba6da7819badbffa5d0a1180780c558
Cosmin Stancu <stancu.cosmin65@gmail.com>
2017-08-17 18:06:05 +0300

    Am schimbat din blog in order si am modificat in orderControler(nu e inca gata)

---
+++ NorthwindWeb/ViewModels/OrderIndexData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using NorthwindWeb.Models;
+
+namespace NorthwindWeb.ViewModels
+{
+    public class OrderIndexData
+    {
+        public IEnumerable<Orders> Order { get; set; }
+        public IEnumerable<Order_Details> Order_Detail { get; set; }
+        public IEnumerable<Products> Product { get; set; }
+    }
+}
