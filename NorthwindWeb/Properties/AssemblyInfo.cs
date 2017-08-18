787f232754c33c29256db7790dd6cd18ee0f566f
SDL Dani <sdl_dani@yahoo.com>
2017-08-18 12:16:25 +0300

    paginare

--- NorthwindWeb/Properties/AssemblyInfo.cs
+++ NorthwindWeb/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NorthwindWeb")]
+[assembly: AssemblyTitle("Studio Phone")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NorthwindWeb")]
+[assembly: AssemblyCompany("Studio Phone Silver")]
+[assembly: AssemblyProduct("Phone")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
