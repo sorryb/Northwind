710be447ba74f42dd697562abea089b26b09f789
Andreea Panait <andreeapanait21@yahoo.com>
2017-08-21 17:11:31 +0300

    numele primelor 4 produse

--- NorthwindWeb/Controllers/ServicesController.cs
+++ NorthwindWeb/Controllers/ServicesController.cs
@@ -1,12 +1,33 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NorthwindWeb.Models;
+using NorthwindWeb.ViewModels;
+using PagedList;
 
 namespace NorthwindWeb.Controllers
 {
     public class ServicesController : Controller
     {
+        private Models.NorthwindModel db = new Models.NorthwindModel();
         public ActionResult Index()
         {
-            return View();
+            var viewModel = new ServicesIndex();
+            viewModel.top4nume = (from p in db.Products
+                                  orderby p.ProductID
+                                  select p.ProductName).Take(4);
+            //var x = from p in db.Products
+            //        orderby p.ProductID
+            //        select p.ProductName;
+            //x=x.Take(4);
+                   
+
+            return View(viewModel);
         }
 	}
 }
---
+++ NorthwindWeb/ViewModels/ServicesIndex.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace NorthwindWeb.ViewModels
+{
+    public class ServicesIndex
+    {
+        public IQueryable<string> top4nume {get; set;}
+       
+    }
+}
