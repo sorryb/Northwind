0a555cd24a72edb6c532d150bfeb9ff428bc0efc
Drăgoi Marius <marius.dragoi0911@yahoo.com>
2017-08-22 11:59:42 +0300

    Search
    Am schimbat view-ul si functionalitatea actiunii search pentru a se potrivi cu noua abordare

---
+++ NorthwindWeb/ViewModels/ViewProductCategoryS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NorthwindWeb.ViewModels
+{
+    public class ViewProductCategoryS
+    {
+        //take ProductID, ProductNmae and CategoryName  all as string
+        public string ProductID { get; set; }   
+        public string ProductName { get; set; }
+        public string CategoryName { get; set; }
+        public string ProductPrice { get; set; }
+    }
+}
