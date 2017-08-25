bc8bdb7b447548a3b2259951ffc1a6e8b313e7d2
SDL Dani <sdl_dani@yahoo.com>
2017-08-25 10:33:23 +0300

    am reparat flexu si am reparat un mic bug la linkurile pt rapoarte

--- NorthwindWeb/Controllers/ReportsController.cs
+++ NorthwindWeb/Controllers/ReportsController.cs
@@ -10,7 +10,7 @@ namespace NorthwindWeb.Controllers
         public ActionResult Index()
         {
 
-            string reportServer = "Reports_SQLEXPRESS";
+            string reportServer = "reportserver_SSRS";
             string dirpath = Path.GetFullPath(Path.Combine(Server.MapPath("~"), @"../NorthwindReports"));
 
             List<ViewModels.ReportViewModel> reports = new List<ViewModels.ReportViewModel>();
@@ -18,7 +18,7 @@ namespace NorthwindWeb.Controllers
             foreach (var filepath in Directory.GetFiles(dirpath, "*rdl"))
             {
                 string filename = Path.GetFileNameWithoutExtension(filepath);
-                string link = "http://localhost/" + reportServer + "/Pages/ReportViewer.aspx?%2fNorthwindReports%2f" + filename + "&rs:Command=Render";
+                string link = "http://localhost/" + reportServer + "/Pages/ReportViewer.aspx?%2fNorthwindReports%2f" + filename.Replace(' ','+') + "&rs:Command=Render";
                 temp = new ViewModels.ReportViewModel(link, filename);
                 reports.Add(temp);
             }
