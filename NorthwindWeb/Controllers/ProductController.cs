5e6ba86f2f6c9491ecc987667eb1cd0802f06d07
SDL Dani <sdl_dani@yahoo.com>
2017-08-28 18:05:27 +0300

    adminS

--- NorthwindWeb/Controllers/ProductController.cs
+++ NorthwindWeb/Controllers/ProductController.cs
@@ -12,7 +12,7 @@ using PagedList;
 
 namespace NorthwindWeb.Controllers
 {
-    [Authorize(Roles = "Admin")]
+    [Authorize(Roles = "Admins")]
     public class ProductController : Controller
     {
         private NorthwindModel db = new NorthwindModel();
