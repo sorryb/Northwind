a3dae8dc9f648b7adb1adda31e2c12899aff9f18
Cosmin Stancu <stancu.cosmin65@gmail.com>
2017-08-28 17:17:24 +0300

    add search in modele

---
+++ NorthwindWeb/ViewModels/Dashboard/LocateSearch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NorthwindWeb.ViewModels.Dashboard
+{
+    public class LocateSearch
+    {
+        public string ID { get; set; }
+        public string WhereFound { get; set; }
+        public string Controller { get; set; }
+        public LocateSearch() { }
+    }
+}
