627a64e3370776ef06cf7b1c0d83bf87d363cbd0
Cosmin Stancu <stancu.cosmin65@gmail.com>
2017-08-29 16:14:38 +0300

    search dashboard add pagination, cate a gasit si index rezultat

---
+++ NorthwindWeb/ViewModels/Dashboard/DashboardSearchData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PagedList;
+
+namespace NorthwindWeb.ViewModels.Dashboard
+{
+    public class DashboardSearchData
+    {
+        public int MatchesCount { get; set; }
+        public IEnumerable<LocateSearch> MatchesFound { get; set; }
+        public IPagedList MatchesFoundPaged { get; set; }
+    }
+}
