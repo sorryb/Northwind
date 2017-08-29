df81892f6673f666686080aab5f43528c633c911
SDL Dani <sdl_dani@yahoo.com>
2017-08-29 17:31:53 +0300

    fu arrows

--- NorthwindWeb/Scripts/product.js
+++ NorthwindWeb/Scripts/product.js
@@ -1,25 +1,30 @@
-﻿
-$(document).ready(function () {
+﻿$(document).ready(function () {
     $('#MyTable').DataTable({
+        responsive: true,
+        "autoWidth": false,
+        columnDefs: [
+            { responsivePriority: 1, targets: 0 },
+            { responsivePriority: 2, targets: -1 }
+        ],
         "ajax": {
             "type": "GET",
             "url": "/Product/JsonTest",
             "dataSrc": function (json) {
                 //Make your callback here.
-                $.each(json, function (index, item){
+                $.each(json, function (index, item) {
                     item.DeleteLink = '<a href= "/Product/Delete?id=' + item.ID + '"/> <i class="fa fa-remove"></i></a >'
                 });
                 return json;
             }
         },
         "columns": [
-            { 'data': 'ProductName' },
-            { 'data': 'Price' },
-            { 'data': 'InStock' },
-            { 'data': 'OnOrders' },
-            { 'data': 'ReorderLevel' },
-            { 'data': 'Discontinued' },
-            { 'data': 'DeleteLink'}
+            { 'data': 'ProductName'  },
+            { 'data': 'Price'                      },
+            { 'data': 'InStock'                         },
+            { 'data': 'OnOrders'                        } ,
+            { 'data': 'ReorderLevel'                     },
+            { 'data': 'Discontinued'                    },
+            { 'data': 'DeleteLink'                   }
         ]
 
     });
