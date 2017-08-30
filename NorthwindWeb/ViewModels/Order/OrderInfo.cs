c5b41fae3d9216e736cb18587320984e5b3aa513
Cosmin Stancu <stancu.cosmin65@gmail.com>
2017-08-30 11:10:08 +0300

    CamelCase

--- NorthwindWeb/ViewModels/Order/OrderInfo.cs
+++ NorthwindWeb/ViewModels/Order/OrderInfo.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace NorthwindWeb.ViewModels
+namespace NorthwindWeb.ViewModels.Order
 {/// <summary>
 /// The class used to store, join between the Orders, Customers and Shippers
 /// </summary>
