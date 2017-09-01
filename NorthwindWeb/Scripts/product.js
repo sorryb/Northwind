41c61fa635cae3d926ebd315793a39299e562c13
Cosmin Stancu <stancu.cosmin65@gmail.com>
2017-09-01 10:38:19 +0300

    Furnizori pe server

--- NorthwindWeb/Scripts/product.js
+++ NorthwindWeb/Scripts/product.js
@@ -150,6 +150,8 @@ $(document).ready(function () {
     });
 
     $('#Suppliers').DataTable({
+        "processing": true,
+        "serverSide": true,
         "responsive": true,
         "autoWidth": false,
         "columnDefs": [
@@ -161,17 +163,15 @@ $(document).ready(function () {
             "url": searchControllerPath() + "/JsonTableFill",
             "dataSrc": function (json) {
                 //Make your callback here.
-                $.each(json, function (index, item) {
-                    item.DeleteLink = '<a href= "' + searchControllerPath() + '/Delete?id=' + item.ID + '"/> <i class="fa fa-remove"></i></a >';
-                    item.CompanyName = '<a href= "' + searchControllerPath() + '/Details?id=' + item.ID + '"/>' + item.CompanyName + '</a >';
+                $.each(json.data, function (index, item) {
+                    item.DeleteLink = '<a href= "' + searchControllerPath() + '/Delete?id=' + item.SupplierID + '"/> <i class="fa fa-remove"></i></a >';
+                    item.CompanyName = '<a href= "' + searchControllerPath() + '/Details?id=' + item.SupplierID + '"/>' + item.CompanyName + '</a >';
                 });
-                return json;
+                return json.data;
             }
         },
         "columns": [
-            {
-                'data': 'CompanyName'
-            },
+            { 'data': 'CompanyName' },
             { 'data': 'ContactName' },
             { 'data': 'ContactTitle' },
             { 'data': 'Address' },
