4357facfd4855808d59bc50cc6dfa3adf1006b84
Simona Iurco <simonaiurco@outlook.com>
2017-09-01 11:50:46 +0300

    rezolvare stergere........

--- NorthwindWeb/Controllers/TerritoriesController.cs
+++ NorthwindWeb/Controllers/TerritoriesController.cs
@@ -8,6 +8,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using NorthwindWeb.Models;
+using NorthwindWeb.Models.ExceptionHandler;
 
 namespace NorthwindWeb.Controllers
 {
@@ -127,31 +128,19 @@ namespace NorthwindWeb.Controllers
         [Authorize(Roles = "Admins")]
         public async Task<ActionResult> DeleteConfirmed(string id)
         {
-
-            //db.Database.SqlQuery<string>($@"
-            //    delete from EmployeeTerritories 
-            //    where EmployeeID in 
-            //    (
-            //        select EmployeeID from EmployeeTerritories
-            //        left join Employees on EmployeeTerritories.EmployeeID=Employees.EmployeeID
-            //        where Employees.EmployeeID={id}
-            //    )
-            //", null)
-
             Territories territories = await db.Territories.FindAsync(id);
-            //var employee = db.EmployeeTerritories.Any(o => o.TerritoriesID == id);
+        
+            try { 
             db.Territories.Remove(territories);
             await db.SaveChangesAsync();
             return RedirectToAction("Index");
+            }
+            catch
+            {
+                throw new DeleteException("Nu poti sterge teritoriul deoarece are constrangeri.");
+            }
 
-            //if TerritoriesID from EmployeeTerritories is different from TerritoriesID from Territories: delete, else return in (Details in Region ) ErrorPage
-            //if (!employee)
-            //{
-            //    db.Territories.Remove(territories);
-            //    await db.SaveChangesAsync();
-            //    return RedirectToAction("Details", "Regions", new { id = id });
-            //}
-            //else { return RedirectToAction("Details", "Regions", new { id = id }); }
+           
         }
 
         protected override void Dispose(bool disposing)
