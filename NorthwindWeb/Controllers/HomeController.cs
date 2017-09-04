58f784a0395bfc7ba653ef47cf4f88b72b24612c
Administrator <Administrator@SBURTOIULAP>
2017-09-04 13:13:50 +0300

    Add unit tests .

--- NorthwindWeb/Controllers/HomeController.cs
+++ NorthwindWeb/Controllers/HomeController.cs
@@ -7,23 +7,41 @@ using NorthwindWeb.Models;
 
 namespace NorthwindWeb.Controllers
 {
+    /// <summary>
+    /// Used from enter page from the site (public site not admin site).
+    /// </summary>
     public class HomeController : Controller
-    { NorthwindModel db = new NorthwindModel();
+    {
+
+        NorthwindModel _northwindDatabase = new NorthwindModel();
+
+        /// <summary>
+        /// First page in the site.
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
-            return View();
+            ViewBag.SiteName = "Northwind Phone Shop";
+
+            return View("Index");
         }
+
+        /// <summary>
+        /// Used to construct the menu.
+        /// </summary>
+        /// <returns></returns>
         [ChildActionOnly]
         public ActionResult Menu()
         {
-            var categori = db.Categories;
-            List<string> list = new List<string>();
-            foreach (var item in categori)
+            var productsCategories = _northwindDatabase.Categories;
+            List<string> listOfCategories = new List<string>();
+            foreach (var item in productsCategories)
             {
-                string x = item.CategoryName;
-                list.Add(x);
+                string categoryName = item.CategoryName;
+                listOfCategories.Add(categoryName);
             }
-            return View(list);
+
+            return View(listOfCategories);
         }
     }
 }
---
+++ UnitTestNorthwindWeb/HomeControllerTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NorthwindWeb.Controllers;
+using NorthwindWeb.Models;
+
+using System.Web.Mvc;
+
+namespace UnitTestNorthwindWeb
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        //Arrange
+        HomeController _homeControllerUnderTest = new HomeController();
+
+        [TestMethod]
+        public void ReturnsIndexView()
+        {
+            //Arrage
+
+            //Act
+            var result = _homeControllerUnderTest.Index() as ViewResult;
+
+            //Assert
+            Assert.AreEqual("Index", result.ViewName);
+
+        }
+
+        [TestMethod]
+        public void ReturnsIndexViewResult()
+        {
+            //Arrage
+
+            //Act
+            var result = _homeControllerUnderTest.Index() as ViewResult;
+
+            //Assert
+
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+
+
+        }
+
+        [TestMethod]
+        public void ReturnsViewBag()
+        {
+            //Arrage
+
+            //Act
+            var result = _homeControllerUnderTest.Index() as ViewResult;
+
+            //Assert
+            Assert.AreEqual("Northwind Phone Shop", result.ViewBag.SiteName);
+        }
+
+        [TestMethod]
+        public void ReturnsMenuCategories()
+        {
+            //Arrage
+
+            //Act
+            var result = _homeControllerUnderTest.Menu() as ViewResult;
+            var model = result.Model;
+
+            //Assert
+            Assert.IsNotNull(model);
+        }
+
+        [TestMethod]
+        public void SampleTest()
+        {
+            //Arrage
+
+            //Act
+
+            //Assert
+            Assert.AreEqual("HomeController", "HomeController");
+        }
+    }
+}
