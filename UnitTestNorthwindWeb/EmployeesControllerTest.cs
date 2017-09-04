adf1d7d058ac1a864767dbdde6ef26f5ba6afad1
SDL Dani <sdl_dani@yahoo.com>
2017-09-04 17:59:17 +0300

    teste create delete edit employees

--- UnitTestNorthwindWeb/EmployeesControllerTest.cs
+++ UnitTestNorthwindWeb/EmployeesControllerTest.cs
@@ -12,7 +12,7 @@ namespace UnitTestNorthwindWeb
     {
         //Arrange
         EmployeesController _EmployeesControllerUnderTest = new EmployeesController();
-        NorthwindModel db = new NorthwindModel();
+        NorthwindModel _db = new NorthwindModel();
 
         /// <summary>
         /// Tests if create returns view.
@@ -33,22 +33,93 @@ namespace UnitTestNorthwindWeb
         /// </summary>
         /// <returns></returns>
         [TestMethod]
-        public async System.Threading.Tasks.Task CreateCreates()
+        public async System.Threading.Tasks.Task CreateCreatesAsync()
         {
             //Arrange
-            Employees EmployeeTest = new Employees() { LastName = "test", FirstName = "test" };
+            Employees employeeTest = new Employees() { EmployeeID = 1000, LastName = "test", FirstName = "test" };
+
             //Act
-            var expected = db.Employees.Count()+1;
-            await _EmployeesControllerUnderTest.Create(EmployeeTest);
-            var actual = db.Employees.Count();
-            var employees = db.Employees.Where(e => e.LastName == EmployeeTest.LastName && e.FirstName == EmployeeTest.FirstName);
+            var expected = _db.Employees.Count() + 1;
+            await _EmployeesControllerUnderTest.Create(employeeTest);
+            var actual = _db.Employees.Count();
+            var employees = _db.Employees.Where(e => e.LastName == employeeTest.LastName && e.FirstName == employeeTest.FirstName);
+
             //Assert
-            Assert.AreEqual(expected,actual);
-            
+            Assert.AreEqual(expected, actual);
+
+            _db.Employees.RemoveRange(employees);
+            _db.SaveChanges();
+        }
+        /// <summary>
+        /// Tests if delete returns view
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async System.Threading.Tasks.Task DeleteReturnsViewAsync()
+        {
+            //Arrange
+            Employees employeeTest = new Employees() { LastName = "test", FirstName = "test" };
+            await _EmployeesControllerUnderTest.Create(employeeTest);
+
+            //Act
+            var result = _EmployeesControllerUnderTest.Delete(employeeTest.EmployeeID);
+
+            //Assert
+            Assert.IsNotNull(result);
 
 
-            db.Employees.RemoveRange(employees);
-            db.SaveChanges();
+
+
+
+            var employees = _db.Employees.Where(e => e.LastName == employeeTest.LastName && e.FirstName == employeeTest.FirstName);
+            _db.Employees.RemoveRange(employees);
+            _db.SaveChanges();
+        }
+        /// <summary>
+        /// Tests if delete deletes
+        /// </summary>
+        [TestMethod]
+        public async System.Threading.Tasks.Task DeleteDeletesAsync()
+        {
+            //Arrange
+            Employees employeeTest = new Employees() { LastName = "test", FirstName = "test" };
+            await _EmployeesControllerUnderTest.Create(employeeTest);
+            int expected = _db.Employees.Count() - 1;
+
+            //Act
+            await _EmployeesControllerUnderTest.DeleteConfirmed(employeeTest.EmployeeID);
+            int actual = _db.Employees.Count();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public async System.Threading.Tasks.Task EditEditsAsync()
+        {
+            //Arrange
+            Employees employeeTest = new Employees() { LastName = "test1", FirstName = "test1" };
+            await _EmployeesControllerUnderTest.Create(employeeTest);
+            _db.Entry(employeeTest).State = System.Data.Entity.EntityState.Added;
+
+            var expectedEmployee = _db.Employees.Find(employeeTest.EmployeeID);
+
+            _db.Dispose();
+            employeeTest.LastName = "test2";
+            employeeTest.FirstName = "test2";
+            _db = new NorthwindModel();
+
+            //Act
+            await _EmployeesControllerUnderTest.Edit(employeeTest);
+            _db.Entry(employeeTest).State = System.Data.Entity.EntityState.Modified;
+            var actualEmployee = _db.Employees.Find(employeeTest.EmployeeID);
+
+            //Assert
+            Assert.AreEqual(expectedEmployee, actualEmployee);
+
+
+            var employees = _db.Employees.Where(e => (e.LastName == "test1" && e.FirstName == "test1") || (e.LastName == "test2" && e.LastName == "test2"));
+            _db.Employees.RemoveRange(employees);
+            _db.SaveChanges();
         }
     }
 }
