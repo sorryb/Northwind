097973566c63e0baefc51f420d302c7af0adac5f
Administrator <Administrator@SBURTOIULAP>
2017-09-12 10:40:15 +0300

    remove using

--- NorthwindWeb/Models/ShopCarts.cs
+++ NorthwindWeb/Models/ShopCarts.cs
@@ -1,10 +1,8 @@
 ﻿namespace NorthwindWeb.Models
 {
-    using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
 
     [Table("Shop Carts")]
     public partial class ShopCarts
