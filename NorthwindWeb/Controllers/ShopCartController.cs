aaa2d7d9781fe9420f3d73ca9a2c91a0c172ca34
Cosmin Stancu <stancu.cosmin65@gmail.com>
2017-09-12 13:46:18 +0300

    formular de creare customers arata mai frumos

--- NorthwindWeb/Controllers/ShopCartController.cs
+++ NorthwindWeb/Controllers/ShopCartController.cs
@@ -381,12 +381,12 @@ namespace NorthwindWeb.Controllers
                     }
                     else
                     {
-                        ModelState.AddModelError("", "Introduceti numarul de telefon");
+                        ModelState.AddModelError("Phone", "Introduceti numarul de telefon");
                     }
                 }
                 else
                 {
-                    ModelState.AddModelError("", "Introduceti adresa");
+                    ModelState.AddModelError("Address", "Introduceti adresa");
                 }
                 
             }
