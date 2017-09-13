1aacc4643fa9670aa4fd4624c2fcfcf70f5c8094
Cosmin Stancu <stancu.cosmin65@gmail.com>
2017-09-13 13:28:05 +0300

    Order coment

--- NorthwindWeb/ViewModels/Order/OrderIndexData.cs
+++ NorthwindWeb/ViewModels/Order/OrderIndexData.cs
@@ -4,15 +4,34 @@ using PagedList;
 
 
 namespace NorthwindWeb.ViewModels.Order
-{/// <summary>
-/// The data model sent by the OrderController to Home
-/// </summary>
+{   /// <summary>
+    /// The data model sent by the OrderController to Home
+    /// </summary>
     public class OrderIndexData
-    {   public IPagedList Page { get; set; }
+    {
+        /// <summary>
+        /// Paging the list of OrderInfo
+        /// </summary>
+        public IPagedList Page { get; set; }
+        /// <summary>
+        /// List of OrderInfo
+        /// </summary>
         public IEnumerable<OrderInfo> Order { get; set; }
+        /// <summary>
+        /// List of OrderProduct
+        /// </summary>
         public IEnumerable<OrderProduct> Order_Detail { get; set; }
+        /// <summary>
+        /// List of ProductCategory
+        /// </summary>
         public IEnumerable<ProductCategory> Product { get; set; }
+        /// <summary>
+        /// List of OrderTen
+        /// </summary>
         public IEnumerable<OrderTen> OrderTen { get; set; }
+        /// <summary>
+        /// order with the highest number of products
+        /// </summary>
         public BigOrder Command { get; set; }
     }
 }
