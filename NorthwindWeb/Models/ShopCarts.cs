bf5df544f49e66ea3ddb4a71f56707097c457d73
Drăgoi Marius <marius.dragoi0911@yahoo.com>
2017-09-13 10:23:47 +0300

    ShopCarts Data Table name changed
    Am modificat nule tabelului din [Shop Carts] in [ShopCarts]

--- NorthwindWeb/Models/ShopCarts.cs
+++ NorthwindWeb/Models/ShopCarts.cs
@@ -4,7 +4,7 @@
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
 
-    [Table("Shop Carts")]
+    [Table("ShopCarts")]
     public partial class ShopCarts
     {
         [Key]
