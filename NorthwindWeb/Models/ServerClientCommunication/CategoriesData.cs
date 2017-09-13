65a0367f7d96c6e09b22429cfdf844fec2e8dcb8
Simona Iurco <simonaiurco@outlook.com>
2017-09-13 11:07:45 +0300

    comentarii in urmatoarele modele:ShipperData,RegionData si CategoriesData ce se gasesc in ServerConnectCommunication

--- NorthwindWeb/Models/ServerClientCommunication/CategoriesData.cs
+++ NorthwindWeb/Models/ServerClientCommunication/CategoriesData.cs
@@ -5,11 +5,25 @@ using System.Web;
 
 namespace NorthwindWeb.Models.ServerClientCommunication
 {
+
+    /// <summary>
+    ///  Contain data of categories that datatable need to draw a table.
+    /// </summary>
     public class CategoriesData
     {
-        
+        /// <summary>
+        /// The ID of category.
+        /// </summary>
         public int ID { get; set; }
+
+        /// <summary>
+        /// The name of category.
+        /// </summary>
         public string CategoryName { get; set; }
+
+        /// <summary>
+        /// The description of category.
+        /// </summary>
         public string Description { get; set; }
     }
 }
