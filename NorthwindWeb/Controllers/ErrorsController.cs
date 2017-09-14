c1690e41631d98c83627fb36e19e1f593dbaf103
SDL Dani <sdl_dani@yahoo.com>
2017-09-14 15:57:37 +0300

    errors controller start

---
+++ NorthwindWeb/Controllers/ErrorsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Diagnostics;
+
+namespace NorthwindWeb.Controllers
+{
+    /// <summary>
+    /// Contains methods that read and show error logs.
+    /// </summary>
+    public class ErrorsController : Controller
+    {
+        /// <summary>
+        /// Displays a page with a table containing all the errors from the NorthwindLog
+        /// </summary>
+        /// <returns>Errors index view</returns>
+        public ActionResult Index()
+        {
+            var test = EventLog.GetEventLogs();
+            
+
+
+            return View();
+        }
+    }
+}
