736747a47668eef76b74cd9cd7b545719f9e15e9
Drăgoi Marius <marius.dragoi0911@yahoo.com>
2017-09-18 15:38:28 +0300

    date de referinta si de test

---
+++ NorthwindWeb/App_Data/SQL/Sales Totals by Amount.sql
@@ -0,0 +1,16 @@
+﻿
+CREATE VIEW "Sales Totals by Amount"
+ AS
+SELECT 
+	"Order Subtotals".Subtotal AS SaleAmount, 
+	Orders.OrderID, 
+	Customers.CompanyName, 
+	Orders.ShippedDate
+FROM 	
+	Customers INNER JOIN 
+		(Orders INNER JOIN "Order Subtotals" ON Orders.OrderID = "Order Subtotals".OrderID) 
+	ON Customers.CustomerID = Orders.CustomerID
+WHERE 
+	("Order Subtotals".Subtotal >2500) AND (Orders.ShippedDate BETWEEN '19900101' And getdate())
+
+
---
+++ NorthwindWeb/App_Data/SQL/Summary of Sales by Quarter.sql
@@ -0,0 +1,17 @@
+﻿
+CREATE VIEW "Summary of Sales by Quarter" 
+AS
+SELECT 
+	Orders.ShippedDate, 
+	Orders.OrderID, 
+	"Order Subtotals".Subtotal
+FROM 
+	Orders INNER JOIN 
+	"Order Subtotals" ON Orders.OrderID = "Order Subtotals".OrderID
+WHERE
+	 Orders.ShippedDate IS NOT NULL
+--ORDER BY
+	-- Orders.ShippedDate
+	
+	
+
