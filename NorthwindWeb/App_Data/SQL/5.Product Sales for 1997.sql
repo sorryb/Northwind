0970bab434cdf2d3eec5bcac5391ca76907a6a5b
Administrator <Administrator@SBURTOIULAP>
2017-09-18 16:26:01 +0300

    add sql.

---
+++ NorthwindWeb/App_Data/SQL/5.Product Sales for 1997.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE VIEW "Product Sales for 1997" 
+AS
+SELECT 
+	Categories.CategoryName, 
+	Products.ProductName, 
+	Sum(CONVERT(money,("Order Details".UnitPrice*Quantity*(1-Discount)/100))*100) AS ProductSales
+FROM
+	 (Categories INNER JOIN Products ON Categories.CategoryID = Products.CategoryID) 
+	INNER JOIN (Orders 
+		INNER JOIN "Order Details" ON Orders.OrderID = "Order Details".OrderID) 
+	ON Products.ProductID = "Order Details".ProductID
+WHERE
+	 (((Orders.ShippedDate) Between '19900101' And getDate()))
+GROUP BY 
+	Categories.CategoryName, 
+	Products.ProductName
+
+
+
---
+++ NorthwindWeb/App_Data/SQL/Products Above Average Price.sql
@@ -0,0 +1,13 @@
+﻿
+CREATE VIEW "Products Above Average Price" 
+AS
+SELECT 
+	Products.ProductName, 
+	Products.UnitPrice
+FROM 
+	Products
+WHERE 
+	Products.UnitPrice>(SELECT AVG(UnitPrice) From Products)
+--ORDER BY Products.UnitPrice DESC
+
+
