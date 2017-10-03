7007e64cdc66cc6bc738916e256ea9a7f246f8c8
Iamsdl <sdl_dani@yahoo.com>
2017-10-03 18:39:39 +0300

    about controller + view

---
+++ NorthwindWeb.Core/Controllers/AboutController.cs
@@ -0,0 +1,32 @@
+﻿using NorthwindWeb.Models;
+using System.Linq;
+using NorthwindWeb.Context;
+using Microsoft.AspNetCore.Mvc;
+using NorthwindWeb.Core.Context;
+
+namespace NorthwindWeb.Controllers
+{
+    /// <summary>
+    /// AboutController. For the page Index from AboutUs 
+    /// </summary>
+    public class AboutController : Controller
+    {
+        NorthwindDatabase db = new NorthwindDatabase(new Microsoft.EntityFrameworkCore.DbContextOptions<NorthwindDatabase>());
+       
+        /// <summary>
+        /// Select the first 6 employees
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+
+            var aboutus = from y in db.Employees
+                          select y;
+            aboutus = aboutus.Take(6);
+
+
+
+            return View(aboutus);
+        }
+	}
+}
